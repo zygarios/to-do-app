dfa5c18e9284753a8b6b8e3632074bdb1f5eb145
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2019-07-03 20:55:39 +0200

    end ToDoApp

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
-const allTasks = document.querySelectorAll('.allTasks');
-
+const tasksList = document.querySelector('.all-tasks');
+let tasks = [...document.querySelectorAll('.task')];
+const tasksCountSpan = document.querySelector('.tasks-count span');
+const addTask = document.querySelector('.add-task');
+const findButton = document.querySelector('.find-task');
+const addTaskInput = document.querySelector('.add-task input');
+const removeBtn = document.querySelectorAll('.task button');
 const removeTask = e => {
-  console.log(e.target.parentNode.remove());
+  e.target.parentNode.remove();
+  tasks = [...document.querySelectorAll('.task')];
+  counter();
 };
 
-document
-  .querySelectorAll('li button')
-  .forEach(item => item.addEventListener('click', removeTask));
+removeBtn.forEach(item => item.addEventListener('click', removeTask));
+
+function counter() {
+  tasksCountSpan.textContent = document.querySelectorAll('.task').length;
+}
+
+function handleAddTask(e) {
+  // tasks = [...document.querySelectorAll('.task')];
+  e.preventDefault();
+  const value = addTaskInput.value.toLowerCase();
+  if (value === '' || value.length < 3) {
+    return alert('Wpisz nazwę zadanie o co najmniej 3 znakach');
+  }
+  const task = document.createElement('li');
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getSeconds();
+
+  task.classList.add('task');
+  task.innerHTML = `${value}<button>Usuń</button><span>${hours}:${
+    minutes < 10 ? '0' + minutes : minutes
+  }, ${time.toLocaleDateString()}</span>`;
+  task.children[0].addEventListener('click', removeTask);
+  tasksList.appendChild(task);
+  tasks = document.querySelectorAll('.task');
+  addTaskInput.value = '';
+  counter();
+}
+
+function handleSearch(e) {
+  e.preventDefault();
+  let value = e.target.value.toLowerCase();
+  let tasksCopy = [...tasks];
+  tasksCopy = tasksCopy.filter(task =>
+    task.textContent.toLowerCase().includes(value)
+  );
+  tasksList.textContent = '';
+  tasksCopy.forEach(item => tasksList.append(item));
+  counter();
+}
+
+function handleModal() {}
+
+addTask.addEventListener('submit', handleAddTask);
+findButton.addEventListener('input', handleSearch);
+counter();
